f5fb4fefbddad6e19cd0106ca021dd62944fd8f8
elliotvilhelm <elliot@pourmand.com>
2022-08-01 19:34:31 -0700

    1-4 24

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-## Midnight Dice Game Probabilities
+# Midnight Dice Game Probabilities
 
-Using a Monte Carlo approach we generate the probability distribution of points in a game of Midnight.
+Using a Monte Carlo approach we generate the probability distribution of points in a game of Midnight. This project started with the desire to answer the question, "What are my odds of rolling a perfect game?". Using a Monte Carlo simulation we were able to answer that question and many more.
+
+- The odds of rolling a perfect game with 6 dice is `~5.2%`.
+- The odds of not qualifying with 6 dice is `~8.3%`.
+
+- The odds of rolling a perfect game with 5 dice is `~8.0%`.
+- The odds of not qualifying with 5 dice is `~19.8%`.
 
 
+# Distributions
+## 6 Dice
+---
+### No Strategy
+![6 dice](./assets/6_dice_no_strategy.png)
+```
+std          4.407029
+min          0.000000
+25%         11.000000
+50%         14.000000
+75%         16.000000
+max         24.000000
+```
+
+### With Strategy
 ![6 dice](./assets/6_dice.png)
 ```
 count  1000000.000000
@@ -16,6 +37,22 @@ max         24.000000
 ```
 
 
+## 5 Dice
+---
+### No Strategy
+![5 dice](./assets/5_dice_no_strategy.png)
+```
+count  1000000.000000
+mean         9.269614
+std          4.486434
+min          0.000000
+25%          7.000000
+50%         10.000000
+75%         12.000000
+max         18.000000
+```
+
+### With Strategy
 ![5 dice](./assets/5_dice.png)
 ```
 count  1000000.000000
@@ -27,11 +64,11 @@ min          0.000000
 75%         16.000000
 max         18.000000
 ```
-### Authors
+## Authors
 - https://github.com/amansourian
 - https://github.com/benjaminykim
 - https://github.com/ElliotVilhelm
 
-### References
+## References
 - https://en.wikipedia.org/wiki/Midnight_(game)
 - https://en.wikipedia.org/wiki/Monte_Carlo_method
--- main.py
+++ main.py
@@ -3,62 +3,62 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
+N_FACES = 6
 
-def game():
-    obj = {
-        "one": False,
-        "four": False,
-        "count": 0,
-        "dice": 5
-    }
+def game(use_strategy = False):
+    one = False
+    four = False
+    count = 0
+    dice = 6
 
-    while obj["dice"] > 0:
+    while dice > 0:
         taken = False
-        rolls = [random.randint(1, 6) for _ in range(obj["dice"])]
+        rolls = [random.randint(1, N_FACES) for _ in range(dice)]
 
-        if (not obj["one"]) and (1 in rolls):
+        if not one and 1 in rolls:
             # cache the first one we find
-            obj["one"] = True
-            obj["dice"] -= 1
+            one = True
+            dice -= 1
             rolls.remove(1)
             taken = True
-        if (not obj["four"]) and (4 in rolls):
+        if not four and 4 in rolls:
             # cache the first four we find
-            obj["four"] = True
-            obj["dice"] -= 1
+            four = True
+            dice -= 1
             rolls.remove(4)
             taken = True
         
-        if obj["dice"] == 0:
+        if dice == 0:
             break
 
-        if (not taken) and (6 not in rolls):
-            # just take max available
-            max_roll = max(rolls)
-            obj["count"] += max_roll
-            obj["dice"] -= 1
-            rolls.remove(max_roll)
-        elif (6 in rolls):
-            if (not obj["one"]) or (not obj["four"]):
-                obj["count"] += 6
-                obj["dice"] -= 1
-                rolls.remove(6)
-            else:
-                # take a random number of 6s
-                # c = Counter(rolls)
-                # for _ in range(random.randint(1, c[6])):
-                #     obj["count"] += 6
-                #     obj["dice"] -= 1
-                #     rolls.remove(6)
-
-                # take all the sixes available
-                while 6 in rolls:
-                    obj["count"] += 6
-                    obj["dice"] -= 1
+        if use_strategy:
+            if not taken and 6 not in rolls:
+                # Take max available
+                max_roll = max(rolls)
+                count += max_roll
+                dice -= 1
+                rolls.remove(max_roll)
+            elif 6 in rolls:
+                if not one or not four:
+                    count += 6
+                    dice -= 1
                     rolls.remove(6)
+                else:
+                    # take all the sixes available
+                    while 6 in rolls:
+                        count += 6
+                        dice -= 1
+                        rolls.remove(6)
+
+        else:
+            if not taken:
+                random_dice = rolls[random.randint(0, len(rolls)-1)]
+                count += random_dice
+                dice -= 1
+                rolls.remove(random_dice)
     
-    if (obj["one"] and obj["four"]):
-        return obj["count"]
+    if one and four:
+        return count
     return 0
 
 
@@ -69,12 +69,13 @@ if __name__ == "__main__":
     for k, v in results_ctr.items():
         results_ctr[k] = (v / len(results)) * 100
 
-    itz_df = pd.DataFrame(results)
+    results_df = pd.DataFrame(results)
 
-    print(itz_df.describe())
+    print(results_df.describe())
     print(results_ctr)
 
     plt.bar(results_ctr.keys(), results_ctr.values())
-    plt.ylabel('percent odds')
-    plt.xlabel('points')
+    plt.grid(True)
+    plt.ylabel('Probability Percentage')
+    plt.xlabel('Points')
     plt.show()
